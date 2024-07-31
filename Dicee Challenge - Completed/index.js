ab86424e46381dce58add0857977e8be6026c3d8
hasnaaADN <146742184+hasnaaADN@users.noreply.github.com>
2024-07-31 08:41:03 +0100

    Update index.js

--- Dicee Challenge - Completed/index.js
+++ Dicee Challenge - Completed/index.js
@@ -1,29 +1,36 @@
+// Génère un nombre aléatoire entre 1 et 6 pour le premier dé
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // Le résultat sera un entier entre 1 et 6
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+// Crée une chaîne de caractères représentant le nom du fichier image correspondant au premier dé
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // Par exemple, "dice3.png" si randomNumber1 est 3
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+// Génère le chemin complet du fichier image correspondant au premier dé
+var randomImageSource = "images/" + randomDiceImage; // Par exemple, "images/dice3.png"
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
+// Sélectionne le premier élément <img> dans le document (dans une page où il y a deux images de dés)
 var image1 = document.querySelectorAll("img")[0];
 
+// Modifie l'attribut "src" du premier élément <img> pour qu'il affiche l'image correspondant au premier dé
 image1.setAttribute("src", randomImageSource);
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// Répète le même processus pour le second dé
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // Génère un nombre aléatoire entre 1 et 6 pour le second dé
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; // Crée la chaîne de caractères représentant le fichier image du second dé
 
+// Modifie l'attribut "src" du second élément <img> pour qu'il affiche l'image correspondant au second dé
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
-//If player 1 wins
+// Compare les deux nombres aléatoires pour déterminer le vainqueur
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) {
+  // Si le premier nombre est plus grand, le joueur 1 gagne
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  // Si le second nombre est plus grand, le joueur 2 gagne
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
+} else {
+  // Si les deux nombres sont égaux, c'est une égalité
   document.querySelector("h1").innerHTML = "Draw!";
 }
